6b3cdf6c9b8058994c2cd4bb898e0496636df8a6
namtroo <cvnamt4@gmail.com>
2023-02-24 09:25:17 +0700

    labwork1

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -44,28 +44,3 @@ def input_marks(student_id, course_id):
 
 # Display a list of students
 
-while(True):
-    print("\n0. Exit")
-    print("1. Input students")                      #option 0-6
-    print("2. Input courses")
-    print("3. Input marks")
-    print("4. List student infor")
-    print("5. List course infor")
-    print("6. Show marks for a given course")
-    choose = int(input("Enter your choice: "))
-    if choose == 0:                                 
-        break
-    elif choose == 1:                               #choose 1 to input student infor
-        input_students_infor()
-    elif choose == 2:                               #choose 2 to input courses infor
-        input_courses_infor()
-    elif choose == 3:                               #choose 3 to input student mark
-        input_marks()
-    elif choose == 4:                               #choose 4 to list student infor
-        list_students()
-    elif choose == 5:                               #choose 5 to list course infor
-        list_courses()
-    elif choose == 6:                               #choose 6 to list student mark
-        list_marks()
-    else:
-        print("Invalid, please choose one obtion above:")
